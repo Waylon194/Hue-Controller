8c4c7f2aafad120775633e83b75a4eeffbf3ed78
Waylon <waylon194@hotmail.com>
2019-11-29 11:13:12 +0100

    Removed Emulator switch
    And all variables + imports

--- HueController/app/src/main/java/com/example/hue_controller/ui/main/SettingsFragment.java
+++ HueController/app/src/main/java/com/example/hue_controller/ui/main/SettingsFragment.java
@@ -3,18 +3,15 @@ package com.example.hue_controller.ui.main;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.Switch;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.example.hue_controller.DataController;
 import com.example.hue_controller.R;
@@ -24,15 +21,12 @@ import static android.content.Context.MODE_PRIVATE;
 public class SettingsFragment extends Fragment {
     private View view;
     private Context context;
-    private Switch emuSwitch;
     private EditText ipEditText;
     private EditText portEditText;
     private EditText userKeyEditText;
-    private Boolean emulating;
     private String ip;
     private String port;
     private String userKey;
-    private static final String emultatingTag = "Emulating";
     private static final String ipTag = "IP";
     private static final String portTag = "Port";
     private static final String userKeyTag = "UserKey";
@@ -46,25 +40,12 @@ public class SettingsFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         this.view = inflater.inflate(R.layout.fragment_settings, container, false);
-
-        this.emuSwitch = view.findViewById(R.id.bridgeEmulatorSwitch);
         this.ipEditText = view.findViewById(R.id.ipText);
         this.portEditText = view.findViewById(R.id.portText);
         this.userKeyEditText = view.findViewById(R.id.userKeyText);
-
-        this.emuSwitch.setChecked(emulating);
         this.ipEditText.setText(this.ip);
         this.portEditText.setText(this.port);
         this.userKeyEditText.setText(this.userKey);
-
-        this.emuSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                emulating = isChecked;
-                saveBoolean(emultatingTag, emulating);
-                DataController.getInstance().refreshLamps();
-            }
-        });
 
         this.ipEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
@@ -109,17 +90,9 @@ public class SettingsFragment extends Fragment {
 
     public void loadPreferences() {
         SharedPreferences preferences = this.context.getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-        this.emulating = preferences.getBoolean(this.emultatingTag, true);
         this.ip = preferences.getString(this.ipTag, "145.48.205.33");
         this.port = preferences.getString(this.portTag, "80");
         this.userKey = preferences.getString(this.userKeyTag, "iYrmsQq1wu5FxF9CPqpJCnm1GpPVylKBWDUsNDhB");
-    }
-
-    private void saveBoolean(String key, boolean value){
-        SharedPreferences.Editor preferences = this.context.getSharedPreferences(PREFS_NAME, MODE_PRIVATE).edit();
-        preferences.putBoolean(key, value);
-        preferences.apply();
-        Log.d("saveBoolean", "Saved " + key + " with " + value);
     }
 
     private void saveString(String key, String value){
